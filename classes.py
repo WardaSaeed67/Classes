2658bdddc11644410f6576ce0dbdfda5aa240a27
WardaSaeed67 <dongshancai642@gmail.com>
2026-02-27 11:21:45 +0500

    Add files via upload

---
+++ classes.py
@@ -0,0 +1,22 @@
+# class Person:
+#   pass
+
+#_________________________________
+# class student:
+#     def __init__(self,name):
+#         self.name=name
+#     def greeting(self):
+#         print("hello",self.name)
+# s1=student("warda")
+# s1.greeting()
+
+#_______________________________________
+
+class person:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+p1=person("warda",20)
+print(p1.name)
+print(p1.age)
+
